c2cc9587cdea27fc3b3545798228c1f4956a5e53
polosa00 <i@apolochanin.ru>
2023-03-25 18:59:16 +0300

    Setup logic for App

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorizedView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.value = 0.05
+        greenSlider.value = 0.27
+        blueSlider.value = 0.49
+        
+        setColorForView()
+        setValueForLabels()
+        
     }
-
-
+    
+    @IBAction func changedRGBColor() {
+        setColorForView()
+        setValueForLabels()
+    }
 }
+
+extension ViewController {
+    private func setColorForView() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValueForLabels() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+}
+
+
 
